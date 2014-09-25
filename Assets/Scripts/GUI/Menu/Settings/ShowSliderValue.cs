7ac6fc89f825f973be6f919bf28497dc3326b1c8
DeadlinezZ <fabrice-95@hotmail.com>
2014-09-25 16:41:36 +0200

    Added GUI-System
    - Added Menu Structure (new ugui)
    - Settings (Graphics, Audio)
    You can now change the resolution and Detail Level
    - Added AudioManager
    - Added GraphicsManager
    - Added ControlsManager

---
+++ Assets/Scripts/GUI/Menu/Settings/ShowSliderValue.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class ShowSliderValue : MonoBehaviour
+{
+
+	public void UpdateLabel (float value)
+	{
+		Text lbl = GetComponent<Text>();
+		if (lbl != null)
+			lbl.text = Mathf.RoundToInt (value * 100) + "%";
+	}
+
+	public void UpdateResolutionLabel(float value){
+		Text lbl = GetComponent<Text>();
+		if (lbl != null){
+			int resIndex = (int)value;
+			lbl.text = Screen.resolutions[resIndex].width + "x" + Screen.resolutions[resIndex].height;
+		}
+	}
+
+	public void UpdateQualityLabel(float value){
+		Text lbl = GetComponent<Text>();
+		if (lbl != null){
+			int level = (int)value;
+			lbl.text = QualitySettings.names[level];
+		}
+	}
+}
---
+++ Assets/Scripts/GraphicsManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GraphicsManager : MonoBehaviour {
+
+	//Graphics Settings
+	private const int defQualityLevel = 3;
+	private int defResolutionIndex;
+	private bool fullscreen = false;
+
+	public GameObject ResolutionSlider;
+	public GameObject QualitySlider; 
+
+
+	void Awake () {
+		defResolutionIndex = Screen.resolutions.Length - 1;
+		ResolutionSlider.GetComponent<Slider>().maxValue = Screen.resolutions.Length - 1;
+		int resIndex = PlayerPrefs.HasKey("resolutionIndex") ? PlayerPrefs.GetInt("resolutionIndex") : defResolutionIndex;
+		ResolutionSlider.GetComponent<Slider>().value = resIndex;
+		Screen.SetResolution (Screen.resolutions[resIndex].width, Screen.resolutions[resIndex].height, fullscreen);
+
+		int level = PlayerPrefs.HasKey("qualityLevel") ? PlayerPrefs.GetInt("qualityLevel") : defQualityLevel;
+		QualitySlider.GetComponent<Slider>().value = level;
+		QualitySettings.SetQualityLevel(level, true);
+
+	}
+	
+	private void SetQualityLevel(){
+		if(GameObject.Find (QualitySlider.name) != null){
+			int level = (int)QualitySlider.GetComponent<Slider>().value;
+			PlayerPrefs.SetInt("qualityLevel", level);
+			QualitySettings.SetQualityLevel(level, true);
+		}
+	}
+
+	private void SetResolution(){
+		if(GameObject.Find (ResolutionSlider.name) != null){
+			int resIndex = (int)ResolutionSlider.GetComponent<Slider>().value;
+			PlayerPrefs.SetInt("resolutionIndex", resIndex);
+			Screen.SetResolution (Screen.resolutions[resIndex].width, Screen.resolutions[resIndex].height, fullscreen);
+
+		}
+	}
+
+	public void SaveAll(){
+		SetQualityLevel();
+		SetResolution();
+	}
+}
